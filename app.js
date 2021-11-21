7c03f11391bc2171541435f8fcf3a862cc982975
Aleksey Bogush <71962603+Bogushukr@users.noreply.github.com>
2021-11-21 22:40:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const createTemplate = (task, index) => {
             <div class="description">${task.title}</div>
             <div class="buttons">
                 <input onclick="completeItem(${index})" type="checkbox" class="btn-complete" ${task.completed ? 'checked' : ''}>
-                <button onclick="deleteItem(${index})" class="btn-delete"><img class="icon" src="/bin.png" alt="Delete"></button>
+                <button onclick="deleteItem(${index})" class="btn-delete"><img class="icon" src="./bin.png" alt="Delete"></button>
             </div>
         </div>
     `
